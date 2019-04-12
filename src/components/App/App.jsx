2490082fff0e52908e149635f17f3d61135aeebd
Brandon Risell <Brandon.Risell@WillowTreeApps.com>
2019-04-11 23:17:53 -0400

    Whoa, styled components are nice

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,17 +1,9 @@
-import React from "react";
-import PropTypes from "prop-types";
 import styled from "styled-components";
 
-const Salutation = styled.h1`
+export const App = styled.h1`
   display: flex;
   align-items: center;
   justify-content: center;
   height: 100%;
   width: 100%;
 `;
-
-export const App = ({ children }) => <Salutation>{children}</Salutation>;
-
-App.propTypes = {
-  children: PropTypes.node.isRequired
-};
